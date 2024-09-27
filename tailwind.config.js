a3df770b5fa28b5f1ac42e72caecb04b2ca9831e
Marius_GBK <mariusbiaou75@gmail.comgit config --global user.email mariusbiaou75@linux.org>
2024-09-27 14:01:53 +0100

    restaurant website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#F03328",
+        secondary:"#FF9E0C",
+        tertiary:"#D58000"
+      },
+    },
+  },
+  plugins: [],
+}
